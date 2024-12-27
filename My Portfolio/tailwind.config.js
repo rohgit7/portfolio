06e5d6a45826d454fa663e5c2c66d7974f656414
R Rohan Shalom <rohanshalom05@gmail.com>
2024-12-27 17:09:24 +0530

    Add files via upload
    Portfolio

---
+++ My Portfolio/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        exo: ['Exo', 'sans-serif'], 
+        rubik: ['Rubik Mono One', 'monospace'], 
+      },
+    },
+  },
+  plugins: [],
+}
+
